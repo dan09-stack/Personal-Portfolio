ec48868a1994fd2f0370aee1eb69aa498fa66e80
dan09 <daro.albis.up@phinmaed.com>
2024-07-06 09:04:25 +0800

    importing all code

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+import Spline from '@splinetool/react-spline/next';
+
+export default function Home() {
+  return (
+    <main>
+      <Spline
+        scene="https://prod.spline.design/VYgIz5eSz6EoR3kX/scene.splinecode" 
+      />
+    </main>
+  );
+}
